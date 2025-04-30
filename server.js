0c881d82fc9c0b454303159e26f23e89ab17191f
kishan <76035974+kishuuuuu@users.noreply.github.com>
2025-04-30 16:05:57 +0530

    server update

--- server.js
+++ server.js
@@ -5,22 +5,18 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// HTTP server
 const server = app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
+// WebSocket server
 const wss = new WebSocket.Server({ server });
 
 const clients = {
   broadcaster: null,
-  viewers: new Map(), // Map of WebSocket -> { id }
+  viewer: null,
 };
-
-function send(ws, data) {
-  if (ws && ws.readyState === WebSocket.OPEN) {
-    ws.send(JSON.stringify(data));
-  }
-}
 
 wss.on("connection", (ws) => {
   console.log("New client connected");
@@ -34,39 +30,25 @@ wss.on("connection", (ws) => {
         clients.broadcaster = ws;
         console.log("Broadcaster registered");
       } else if (data.type === "viewer") {
-        const viewerId =
-          Date.now() + "_" + Math.random().toString(36).substr(2, 5);
-        ws.viewerId = viewerId;
-        clients.viewers.set(ws, { id: viewerId });
-
-        console.log("Viewer registered:", viewerId);
-
-        // Notify broadcaster about new viewer
-        send(clients.broadcaster, {
-          type: "new-viewer",
-          from: viewerId,
-        });
+        clients.viewer = ws;
+        console.log("Viewer registered");
       } else if (data.type === "signal") {
-        const { from, target, data: signalData } = data;
+        const target =
+          data.target === "broadcaster"
+            ? clients.broadcaster
+            : data.target === "viewer"
+            ? clients.viewer
+            : null;
 
-        if (target === "broadcaster") {
-          send(clients.broadcaster, {
-            type: "signal",
-            from,
-            data: signalData,
-          });
+        if (target && target.readyState === WebSocket.OPEN) {
+          target.send(
+            JSON.stringify({
+              type: "signal",
+              data: data.data,
+            })
+          );
         } else {
-          // Send to correct viewer
-          for (let [viewerWs, info] of clients.viewers.entries()) {
-            if (info.id === target) {
-              send(viewerWs, {
-                type: "signal",
-                from,
-                data: signalData,
-              });
-              break;
-            }
-          }
+          console.warn("Target not available or not open");
         }
       }
     } catch (err) {
@@ -78,13 +60,14 @@ wss.on("connection", (ws) => {
     if (ws === clients.broadcaster) {
       console.log("Broadcaster disconnected");
       clients.broadcaster = null;
-    } else if (clients.viewers.has(ws)) {
-      console.log("Viewer disconnected:", ws.viewerId);
-      clients.viewers.delete(ws);
+    } else if (ws === clients.viewer) {
+      console.log("Viewer disconnected");
+      clients.viewer = null;
     }
   });
 });
 
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/broadcast", (req, res) => {
